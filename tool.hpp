251e82e4a61274d023cf87aa640b67b007419ee0
flowmemo <flowmemo@outlook.com>
2017-04-17 00:35:20 +0800

    add hash function from boost

--- tool.hpp
+++ tool.hpp
@@ -1,27 +1,16 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <set>
+#include <sstream>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
-#include <cmath>
 #include <numeric>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <iomanip>
-/*
-// custom hash function
-namespace std {
-template <>
-struct hash<pair<int, int>>> {
-  size_t operator()(const pair<int,int> &k) const {
-    return hash<int>()(k.first) ^ hash<int>()(k.second);
-  }
-};
-}
-*/
 
 namespace tool {
 
@@ -113,4 +102,36 @@ void iota(ForwardIter first, ForwardIter last, T val) {
     *first++ = val++;
   }
 }
+
+
+// https://github.com/boostorg/functional/blob/develop/include/boost/functional/hash/hash.hpp
+// https://github.com/boostorg/functional/blob/develop/include/boost/functional/hash/extensions.hpp
+template <typename SizeT>
+inline void hash_combine_impl(SizeT& seed, SizeT value) {
+  seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template <class T>
+inline void hash_combine(std::size_t& seed, T const& v) {
+  hash<T> hasher;
+  return hash_combine_impl(seed, hasher(v));
+}
+
+template <class A, class B>
+std::size_t hash_value(std::pair<A, B> const& v) {
+  std::size_t seed = 0;
+  hash_combine(seed, v.first);
+  hash_combine(seed, v.second);
+  return seed;
+}
+}
+
+// custom hash function
+namespace std {
+template <typename T1, typename T2>
+struct hash<pair<T1, T2>> {
+  size_t operator()(const pair<T1, T2> &k) const {
+    return hash_value(k);
+  }
+};
 }
