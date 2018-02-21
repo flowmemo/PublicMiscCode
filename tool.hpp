525bf30589e0026f84c1d187f87cfd37bde48cb5
flowmemo <flowmemo@outlook.com>
2018-02-21 21:10:05 +0800

    add frequently used code

--- tool.hpp
+++ tool.hpp
@@ -16,6 +16,16 @@
 #include <vector>
 
 /*
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+int main () {
+  ios::sync_with_stdio(false);
+}
+*/
+
+/*
 -Wall -Wextra -pedantic -std=c++14 -O2 -Wshadow -Wformat=2 -Wfloat-equal \
 -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual \
 -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 \
