bb666974d1ff739e124faa446e8bd325658d226e
flowmemo <flowmemo@outlook.com>
2018-03-18 00:10:53 +0800

    fvisibility=hidden

--- tool.hpp
+++ tool.hpp
@@ -30,7 +30,7 @@ int main () {
 -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual \
 -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 \
 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover \
--fstack-protector -Wfatal-errors
+-fstack-protector -Wfatal-errors -fvisibility=hidden
 Ref: http://codeforces.com/blog/entry/15547?locale=en
 */
 
